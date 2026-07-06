f5af4462a2f222ce00d922e4696dd7882943b7d2
Ammar Ahmed <ammarahmed1263@gmail.com>
2026-07-07 02:24:52 +0300

    Fallback to images[0].url if thumbnail is missing in cart item

--- src/components/common/commerce/CartItem.vue
+++ src/components/common/commerce/CartItem.vue
@@ -39,7 +39,7 @@ async function remove() {
         class="w-20 h-20 rounded-xl overflow-hidden bg-muted/30 border border-border/50"
       >
         <img
-          :src="item.product.thumbnail.url"
+          :src="item.product.thumbnail?.url || item.product.images?.[0]?.url"
           :alt="item.product.name"
           loading="lazy"
           class="w-full h-full object-cover hover:scale-105 transition-transform duration-300"
